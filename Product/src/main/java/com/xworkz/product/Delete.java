3232b0986d57d9d9c7529991c899984b6ccd903d
Amruthamudhol <amrutashokmudhol24@gmail.com>
2026-06-12 07:27:57 +0530

    adding jdbc file

--- Product/src/main/java/com/xworkz/product/Delete.java
+++ Product/src/main/java/com/xworkz/product/Delete.java
@@ -1,4 +1,28 @@
 package com.xworkz.product;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Delete {
+
+    public static void main(String[] args) {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/product_db", "root", "4AI22CS005");
+            Statement statement = connection.createStatement();
+            String sqlQuery = "delete from product_info where product_id = 108";
+            int rowsAffected = statement.executeUpdate(sqlQuery);
+
+            System.out.println("Rows Deleted: " + rowsAffected);
+
+            statement.close();
+            connection.close();
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
