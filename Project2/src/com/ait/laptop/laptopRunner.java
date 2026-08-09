cd153b141e94682361d780cb0d89b1802834c903
Amruta Mudhol <amrutashokmudhol24@gmail.com>
2026-08-09 19:28:16 +0530

    Delete Project2 directory

--- Project2/src/com/ait/laptop/laptopRunner.java
+++
@@ -1,8 +0,0 @@
-package com.ait.laptop;
-
-public class laptopRunner {
-    public static void main(String[] args) {
-        LaptopDetails laptopDetails = new LaptopDetails("hp","i5",8,55000);
-        laptopDetails.printDetails();
-    }
-}
